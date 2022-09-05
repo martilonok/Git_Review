c04485145dd7a378d13bb1e913c4c0ae725b8d03
Aleksandr <martilonok@gmail.com>
2022-09-05 17:34:44 -0500

    Made some changes to Hello 1 World .java

--- src/first/Hello1World.java
+++ src/first/Hello1World.java
@@ -3,5 +3,7 @@ package first;
 public class Hello1World {
     public static void main(String[] args) {
         System.out.println("Hello 1 World");
+
+        //made some changes to Hello 1 World
     }
 }
