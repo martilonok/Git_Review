94014ae5e5dfece3c88aa1062e243681f3ef76b5
Aleksandr <martilonok@gmail.com>
2022-09-05 17:37:09 -0500

    made changes to bye world.java

--- src/first/ByeWorld.java
+++ src/first/ByeWorld.java
@@ -3,5 +3,6 @@ package first;
 public class ByeWorld {
     public static void main(String[] args) {
         System.out.println("Bye World");
+        System.out.println("About 5 weeks of java left");
     }
 }
